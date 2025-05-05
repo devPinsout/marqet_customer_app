3922e4aab00032f45e6f19ef7352d2b35001b6c6
Shalini yadav <shaliniyadav0203@gmail.com>
2025-05-05 16:24:19 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.ts
+import withPWA from 'next-pwa';
+import type { NextConfig } from 'next';
+
+// Check if the environment is development
+const isDev = process.env.NODE_ENV === 'development';
+
+// Next.js configuration
 const nextConfig: NextConfig = {
-  /* config options here */
+  // reactStrictMode: true,
+  // Add other Next.js config options here
+  // webpack(config) {
+  //   // This is the right place to modify Webpack configuration
+  //   return config;
+  // }
 };
+// Export the configuration wrapped with PWA support
+export default withPWA({
+  ...nextConfig, // Spread the nextConfig to include all the original options
+  dest: 'public', // Specifies where the service worker files are stored
+  disable: isDev, // Disable PWA in development mode
+  register: true, // Automatically register the service worker
+  skipWaiting: true, // Skip waiting when the new service worker is ready
+});
 
-export default nextConfig;
+
+
+
+
